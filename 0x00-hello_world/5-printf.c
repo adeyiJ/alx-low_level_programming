254af0bb82e94d75608525811b6f165dceb38455
adeyiJ <adeyijoshua.aj@gmail.com>
2022-10-16 15:51:36 -0700

    commit uising printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -using main function printf to print and return 0
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
