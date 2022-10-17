5b8480e048b6381b2cea4d611ed42fda5db5016c
adeyiJ <adeyijoshua.aj@gmail.com>
2022-10-17 02:53:49 -0700

    printing data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ ** main - a function describing how to printing out size of data type in c
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a))
+	printf("size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b))
+	printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c))
+	printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d))
+	printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
